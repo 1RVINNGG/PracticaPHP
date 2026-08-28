f0bd56c21412f3422bbb00e566bede1395c30af1
Irving Cruz <irvingcruz2728@gmail.com>
2026-08-28 08:52:52 -0500

    actualizadoo

--- variables.php
+++ variables.php
@@ -1,6 +1,14 @@
-<?php
-$author1 = "John Doe";
-$author2 = "Max Mustermann";
-echo "<h1>Hello World!</h1> 
-<p>This dynamic web page was created by $author1 and $author2.</p>";
+<?php 
+
+// Cadena de texto (string) que contiene el nombre del primer autor
+$author1 = "John Doe"; 
+
+// Cadena de texto (string) que contiene el nombre del segundo autor
+$author2 = "Max Mustermann"; 
+
+// Mostramos un título y un párrafo en la página web.
+// Se utilizan las cadenas almacenadas en $author1 y $author2.
+echo "<h1>Hello World!</h1>  
+<p>This dynamic web page was created by $author1 and $author2.</p>"; 
+
 ?>
