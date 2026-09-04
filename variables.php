9f6bf1293fc28bb3b95ce5281863883429d9f3b5
Irving Cruz <irvingcruz2728@gmail.com>
2026-09-04 09:10:53 -0500

    Con css

--- variables.php
+++ variables.php
@@ -1,14 +1,32 @@
-<?php 
 
-// Cadena de texto (string) que contiene el nombre del primer autor
-$author1 = "John Doe"; 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
 
-// Cadena de texto (string) que contiene el nombre del segundo autor
-$author2 = "Max Mustermann"; 
+    <!-- Enlazamos el archivo CSS -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<?php  
+
+// Cadena de texto (string) que contiene el nombre del primer autor 
+$author1 = "John Doe";  
+
+// Cadena de texto (string) que contiene el nombre del segundo autor 
+$author2 = "Max Mustermann";  
 
 // Mostramos un título y un párrafo en la página web.
 // Se utilizan las cadenas almacenadas en $author1 y $author2.
-echo "<h1>Hello World!</h1>  
-<p>This dynamic web page was created by $author1 and $author2.</p>"; 
+echo "<div class='contenedor'>
+        <h1>Hello World!</h1>
+        <p>This dynamic web page was created by $author1 and $author2.</p>
+      </div>";
 
 ?>
+
+</body>
+</html>
+```
